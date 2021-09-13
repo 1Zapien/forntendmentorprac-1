be141ec129b1325bd04bead1d1bb8ffc505ff16e
1Zapien <guan235@gmail.com>
2021-09-12 22:49:56 -0700

    changes in UI

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const form = document.getElementById("fillForm");
+const form = document.getElementById("reset");
+
+document.getElementById("reset").onclick = ()=>{
+    document.getElementById("fillForm").reset();
+};
